ab5e607a250a08fb3c46f70d3f892517f8caa40f
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-13 20:08:44 +0300

    Kategoriler formu tamamlandı

--- Lecture_28-FinancialCrm/Forms/FrmCategory.Designer.cs
+++ Lecture_28-FinancialCrm/Forms/FrmCategory.Designer.cs
@@ -39,8 +39,21 @@
             this.btnBills = new System.Windows.Forms.Button();
             this.btnBanks = new System.Windows.Forms.Button();
             this.btnCategories = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dgvCategories = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategories)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
@@ -174,11 +187,123 @@
             this.btnCategories.Text = "Kategoriler";
             this.btnCategories.UseVisualStyleBackColor = true;
             // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnDelete);
+            this.panel3.Controls.Add(this.btnUpdate);
+            this.panel3.Controls.Add(this.btnAdd);
+            this.panel3.Controls.Add(this.txtName);
+            this.panel3.Controls.Add(this.txtID);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.panel3.Location = new System.Drawing.Point(182, 56);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(613, 130);
+            this.panel3.TabIndex = 8;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.dgvCategories);
+            this.panel4.Location = new System.Drawing.Point(182, 192);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(613, 241);
+            this.panel4.TabIndex = 9;
+            // 
+            // dgvCategories
+            // 
+            this.dgvCategories.AllowUserToAddRows = false;
+            this.dgvCategories.AllowUserToDeleteRows = false;
+            this.dgvCategories.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCategories.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategories.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCategories.Location = new System.Drawing.Point(0, 0);
+            this.dgvCategories.Name = "dgvCategories";
+            this.dgvCategories.ReadOnly = true;
+            this.dgvCategories.Size = new System.Drawing.Size(613, 241);
+            this.dgvCategories.TabIndex = 0;
+            this.dgvCategories.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCategories_CellDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(24, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Kategori Adı: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(30, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kategori ID: ";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(133, 27);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(169, 26);
+            this.txtID.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(133, 59);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(169, 26);
+            this.txtName.TabIndex = 3;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDelete.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnDelete.Location = new System.Drawing.Point(405, 89);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(175, 37);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnUpdate.Location = new System.Drawing.Point(405, 46);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(175, 37);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAdd.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnAdd.Location = new System.Drawing.Point(405, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(175, 37);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
             // FrmCategory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(807, 445);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
@@ -187,9 +312,14 @@
             this.Name = "FrmCategory";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmCategory";
+            this.Load += new System.EventHandler(this.FrmCategory_Load);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategories)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -207,5 +337,15 @@
         private System.Windows.Forms.Button btnBills;
         private System.Windows.Forms.Button btnBanks;
         private System.Windows.Forms.Button btnCategories;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.DataGridView dgvCategories;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
--- Lecture_28-FinancialCrm/Forms/FrmCategory.cs
+++ Lecture_28-FinancialCrm/Forms/FrmCategory.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using Lecture_28_FinancialCrm.Models;
+using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Lecture_28_FinancialCrm
 {
     public partial class FrmCategory : Form
     {
+        FinancialCrmEntities db = new FinancialCrmEntities();
+
         public FrmCategory()
         {
             InitializeComponent();
+        }
+
+        private void ClearForm()
+        {
+            txtID.Clear();
+            txtName.Clear();
+        }
+
+        private void LoadCategories()
+        {
+            dgvCategories.DataSource = db.Categories.Select(x => new
+            {
+                ID = x.CategoryId,
+                Kategori_Adı = x.Name
+            }).ToList();
         }
 
         private void btnExit_Click(object sender, EventArgs e)
@@ -47,6 +66,58 @@ namespace Lecture_28_FinancialCrm
         private void btnSettings_Click(object sender, EventArgs e)
         {
             FormNavigator.Navigate(this, new FrmSettings());
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                MessageBox.Show("Kategori adı boş bırakılamaz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Categories category = new Categories
+            {
+                Name = txtName.Text
+            };
+            db.Categories.Add(category);
+            db.SaveChanges();
+            MessageBox.Show("Kategori Başarıyla Eklendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ClearForm();
+            LoadCategories();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtID.Text);
+            Categories category = db.Categories.Find(id);
+            category.Name = txtName.Text;
+            db.SaveChanges();
+            MessageBox.Show("Kategori Başarıyla Güncellendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ClearForm();
+            LoadCategories();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtID.Text);
+            Categories category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            MessageBox.Show("Kategori Başarıyla Silindi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ClearForm();
+            LoadCategories();
+        }
+
+        private void FrmCategory_Load(object sender, EventArgs e)
+        {
+            LoadCategories();
+        }
+
+        private void dgvCategories_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectedRow = dgvCategories.SelectedCells[0].RowIndex;
+            txtID.Text = dgvCategories.Rows[selectedRow].Cells[0].Value.ToString();
+            txtName.Text = dgvCategories.Rows[selectedRow].Cells[1].Value.ToString();
         }
     }
 }
