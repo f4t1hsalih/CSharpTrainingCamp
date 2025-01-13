a6457b4166bab76eea6dddc07a0b79b302b92b55
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-13 21:31:05 +0300

    Giderler formu tamamlandı.

--- Lecture_28-FinancialCrm/Forms/FrmSpending.Designer.cs
+++ Lecture_28-FinancialCrm/Forms/FrmSpending.Designer.cs
@@ -39,8 +39,26 @@
             this.btnBills = new System.Windows.Forms.Button();
             this.btnBanks = new System.Windows.Forms.Button();
             this.btnCategories = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dgvSpending = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.nudAmount = new System.Windows.Forms.NumericUpDown();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSpending)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
@@ -174,11 +192,163 @@
             this.btnCategories.UseVisualStyleBackColor = true;
             this.btnCategories.Click += new System.EventHandler(this.btnCategories_Click);
             // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.dgvSpending);
+            this.panel4.Location = new System.Drawing.Point(182, 204);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(613, 229);
+            this.panel4.TabIndex = 13;
+            // 
+            // dgvSpending
+            // 
+            this.dgvSpending.AllowUserToAddRows = false;
+            this.dgvSpending.AllowUserToDeleteRows = false;
+            this.dgvSpending.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSpending.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSpending.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSpending.Location = new System.Drawing.Point(0, 0);
+            this.dgvSpending.Name = "dgvSpending";
+            this.dgvSpending.ReadOnly = true;
+            this.dgvSpending.Size = new System.Drawing.Size(613, 229);
+            this.dgvSpending.TabIndex = 0;
+            this.dgvSpending.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSpending_CellDoubleClick);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.nudAmount);
+            this.panel3.Controls.Add(this.cmbCategory);
+            this.panel3.Controls.Add(this.btnDelete);
+            this.panel3.Controls.Add(this.btnUpdate);
+            this.panel3.Controls.Add(this.btnAdd);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.txtTitle);
+            this.panel3.Controls.Add(this.txtID);
+            this.panel3.Location = new System.Drawing.Point(182, 56);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(613, 142);
+            this.panel3.TabIndex = 12;
+            // 
+            // nudAmount
+            // 
+            this.nudAmount.Location = new System.Drawing.Point(96, 75);
+            this.nudAmount.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.nudAmount.Name = "nudAmount";
+            this.nudAmount.Size = new System.Drawing.Size(236, 26);
+            this.nudAmount.TabIndex = 13;
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(96, 107);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(236, 28);
+            this.cmbCategory.TabIndex = 12;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDelete.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnDelete.Location = new System.Drawing.Point(383, 97);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(175, 37);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnUpdate.Location = new System.Drawing.Point(383, 54);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(175, 37);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAdd.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnAdd.Location = new System.Drawing.Point(383, 11);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(175, 37);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ID:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Kategori:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Başlık:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tutar:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(96, 43);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(236, 26);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(96, 11);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(236, 26);
+            this.txtID.TabIndex = 1;
+            // 
             // FrmSpending
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(807, 445);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
@@ -187,9 +357,15 @@
             this.Name = "FrmSpending";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmSpending";
+            this.Load += new System.EventHandler(this.FrmSpending_Load);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSpending)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -207,5 +383,19 @@
         private System.Windows.Forms.Button btnBills;
         private System.Windows.Forms.Button btnBanks;
         private System.Windows.Forms.Button btnCategories;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.DataGridView dgvSpending;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.NumericUpDown nudAmount;
     }
 }
--- Lecture_28-FinancialCrm/Forms/FrmSpending.cs
+++ Lecture_28-FinancialCrm/Forms/FrmSpending.cs
@@ -1,13 +1,92 @@
-﻿using System;
+﻿using Lecture_28_FinancialCrm.Models;
+using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Lecture_28_FinancialCrm
 {
     public partial class FrmSpending : Form
     {
+        FinancialCrmEntities db = new FinancialCrmEntities();
+
         public FrmSpending()
         {
             InitializeComponent();
+        }
+
+        private void ClearForm()
+        {
+            txtID.Clear();
+            txtTitle.Clear();
+            nudAmount.Value = 0;
+            cmbCategory.SelectedIndex = 0;
+        }
+
+        private void LoadData()
+        {
+            var data = db.Spendings.Select(x => new
+            {
+                ID = x.SpendingId,
+                Kategori = x.Categories.Name,
+                Başlık = x.Title,
+                Tutar = x.Amount,
+                Tarih = x.Date,
+            }).ToList();
+            dgvSpending.DataSource = data;
+        }
+
+        private void LoadComboBox()
+        {
+            var categories = db.Categories.ToList();
+            cmbCategory.DataSource = categories;
+            cmbCategory.DisplayMember = "Name";
+            cmbCategory.ValueMember = "CategoryId";
+        }
+
+        private void AddSpending()
+        {
+            if (txtTitle.Text == "" || nudAmount.Value == 0)
+            {
+                MessageBox.Show("Lütfen Tüm Alanları Doldurunuz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Spendings spending = new Spendings();
+                spending.CategoryId = (int)cmbCategory.SelectedValue;
+                spending.Title = txtTitle.Text;
+                spending.Amount = nudAmount.Value;
+                spending.Date = DateTime.Now.Date;
+                db.Spendings.Add(spending);
+                db.SaveChanges();
+                ClearForm();
+                LoadData();
+            }
+        }
+
+        private void UpdateSpending(int id)
+        {
+            if (txtTitle.Text == "" || nudAmount.Value == 0)
+            {
+                MessageBox.Show("Lütfen Tüm Alanları Doldurunuz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Spendings spendings = db.Spendings.Find(id);
+                spendings.Title = txtTitle.Text;
+                spendings.Amount = nudAmount.Value;
+                spendings.CategoryId = (int)cmbCategory.SelectedValue;
+                db.SaveChanges();
+                ClearForm();
+                LoadData();
+            }
+        }
+
+        private void DeleteSpending(int id)
+        {
+            Spendings spending = db.Spendings.Find(id);
+            db.Spendings.Remove(spending);
+            db.SaveChanges();
+            LoadData();
         }
 
         private void btnExit_Click(object sender, EventArgs e)
@@ -47,6 +126,46 @@ namespace Lecture_28_FinancialCrm
         private void btnSettings_Click(object sender, EventArgs e)
         {
             FormNavigator.Navigate(this, new FrmSettings());
+        }
+
+        private void FrmSpending_Load(object sender, EventArgs e)
+        {
+            LoadData();
+            LoadComboBox();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddSpending();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text.Length == 0)
+            {
+                MessageBox.Show("Lütfen Bir Harcama Seçiniz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            UpdateSpending(Convert.ToInt32(txtID.Text));
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text.Length == 0)
+            {
+                MessageBox.Show("Lütfen Bir Harcama Seçiniz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            DeleteSpending(Convert.ToInt32(txtID.Text));
+        }
+
+        private void dgvSpending_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectedRow = dgvSpending.SelectedCells[0].RowIndex;
+            txtID.Text = dgvSpending.Rows[selectedRow].Cells[0].Value.ToString();
+            txtTitle.Text = dgvSpending.Rows[selectedRow].Cells[2].Value.ToString();
+            nudAmount.Value = Convert.ToDecimal(dgvSpending.Rows[selectedRow].Cells[3].Value);
+            cmbCategory.Text = dgvSpending.Rows[selectedRow].Cells[1].Value.ToString();
         }
     }
 }
